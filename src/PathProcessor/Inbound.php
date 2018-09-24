3f1575991c07768594cf5aef4a1244d12cbe278f
Peter Clarke <peter_clarke@dennis.co.uk>
2018-09-24 14:04:36 +0100

    No need for the kernel event subscriber

--- src/PathProcessor/Inbound.php
+++ src/PathProcessor/Inbound.php
@@ -4,7 +4,9 @@ namespace Drupal\term_node\PathProcessor;
 
 use Drupal\Core\Path\AliasManagerInterface;
 use Drupal\Core\PathProcessor\InboundPathProcessorInterface;
+use Drupal\term_node\NodeResolverInterface;
 use Drupal\term_node\ResolverInterface;
+use Drupal\term_node\TermResolverInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
@@ -14,7 +16,7 @@ use Symfony\Component\HttpKernel\KernelEvents;
 /**
  * Processes the inbound path using path alias lookups.
  */
-class Inbound implements InboundPathProcessorInterface, EventSubscriberInterface {
+class Inbound implements InboundPathProcessorInterface {
 
   /**
    * The core alias manager.
@@ -24,11 +26,18 @@ class Inbound implements InboundPathProcessorInterface, EventSubscriberInterface
   protected $aliasManager;
 
   /**
-   * Handles changing the path if needed.
+   * Figures out if a different path should be used.
    *
-   * @var \Drupal\term_node\PathProcessor\InboundPathInterface
+   * @var TermResolverInterface
    */
-  protected $inboundPath;
+  protected $termResolver;
+
+  /**
+   * Figures out if a different path should be used.
+   *
+   * @var NodeResolverInterface
+   */
+  protected $nodeResolver;
 
   /**
    * Constructs a Inbound object.
@@ -40,49 +49,45 @@ class Inbound implements InboundPathProcessorInterface, EventSubscriberInterface
    */
   public function __construct(
     AliasManagerInterface $alias_manager,
-    InboundPathInterface $inbound_path
+    TermResolverInterface $term_resolver,
+    NodeResolverInterface $node_resolver
   ) {
     $this->aliasManager = $alias_manager;
-    $this->inboundPath = $inbound_path;
+    $this->termResolver = $term_resolver;
+    $this->nodeResolver = $node_resolver;
   }
 
   /**
    * {@inheritdoc}
    */
   public function processInbound($path, Request $request) {
-    return $this->inboundPath->process($path, $request);
-  }
-
-  /**
-   * Set the path ready for processInbound() and disable redirecting if the path changes.
-   *
-   * Has to be done in the kernel request event as the RouteNormalizerRequestSubscriber
-   * performs the redirect on the kernel request event. This therefore has to
-   * run before RouteNormalizerRequestSubscriber::onKernelRequestRedirect()
-   * to disable the redirect, if needed, before it happens.
-   *
-   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
-   */
-  public function onKernelRequest(GetResponseEvent $event) {
-    $request = $event->getRequest();
-
     // Get the internal path.
     $alias = $request->getPathInfo();
     $alias = $alias === '/' ? $alias : rtrim($request->getPathInfo(), '/');
-    $path = $this->aliasManager->getPathByAlias($alias);
+    $internal_path = $this->aliasManager->getPathByAlias($alias);
 
-    // See if the path needs changing.
-    $this->inboundPath->process($path, $request);
-  }
+    $parts = explode('/', trim($internal_path, '/'));
+    $count = count($parts);
+    if ($count == 2 && $parts[0] == 'node') {
+      // If the node is a term_node, do not redirect to the term path
+      // when using the node's own path.
+      if ($this->nodeResolver->getReferencedBy($parts[1])) {
+        // Don't redirect.
+        $request->attributes->add(['_disable_route_normalizer' => TRUE]);
+      }
+    }
+    elseif ($count == 3 && $parts[1] == 'term') {
+      // If the term has node referenced, show the node content
+      // but do not redirect to the node itself.
+      $new_path = $this->termResolver->getPath($path, $parts[2]);
+      if ($new_path != $path) {
+        $path = $this->path = $new_path;
+        // Don't redirect due to the path changing.
+        $request->attributes->add(['_disable_route_normalizer' => TRUE]);
+      }
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public static function getSubscribedEvents() {
-    // Must happen before
-    // Drupal\redirect\EventSubscriber\RouteNormalizerRequestSubscriber::onKernelRequestRedirect().
-    $events[KernelEvents::REQUEST][] = ['onKernelRequest', 50];
-    return $events;
+    return $path;
   }
 
 }
